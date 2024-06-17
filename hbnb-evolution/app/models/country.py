6a48b3e158e45f2f1a43b7bcef46b15b9455040a
Ibrux11 <ibraluunge@gmail.com>
2024-06-17 14:06:49 +0200

    correction contry.py

--- hbnb-evolution/app/models/country.py
+++ hbnb-evolution/app/models/country.py
@@ -1,3 +1,29 @@
-#!/user/bin/python3
+#!/usr/bin/python3
+"""Module for Country class"""
 
-def __init__(name,code):
+from models.base_model import BaseModel
+
+
+class Country(BaseModel):
+    """Country class that inherits from BaseModel"""
+
+    def __init__(self, name, code, *args, **kwargs):
+        """
+        Initializes the class Country with the following parameters:
+        :param name: str - Name of the Country.
+        :param code: str - The Country international code.
+        """
+        super().__init__(*args, **kwargs)
+        self.name = name
+        self.code = code
+
+    def to_dict(self):
+        """
+        Converts the Country instance to a dictionary representation.
+        """
+        result = super().to_dict()
+        result.update({
+            "name": self.name,
+            "code": self.code,
+        })
+        return result
