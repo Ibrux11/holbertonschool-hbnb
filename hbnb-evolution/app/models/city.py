dc08288d8b08a7f14952d783d5d5e1a4b8611984
Ibrux11 <ibraluunge@gmail.com>
2024-06-17 10:57:38 +0200

    correction for models city

--- hbnb-evolution/app/models/city.py
+++ hbnb-evolution/app/models/city.py
@@ -1,1 +1,12 @@
-#!/user/bin/python3
+#!/usr/bin/python3
+"""Module for City class"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """City class that inherits from BaseModel."""
+
+    name = ""
+    country_id = ""
+ 
